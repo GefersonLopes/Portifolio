6e21f77028842c7cca94507dfc89717513c8278d
Geferson <gefersonjefreey@gmail.com>
2023-01-18 18:02:53 -0300

    feat: conclusion project protfoli

---
+++ src/utils/stackData.ts
@@ -0,0 +1,92 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiPostgresql, SiTypescript } from "react-icons/si";
+import { FaGit, FaPython } from "react-icons/fa";
+import {
+    SiMongodb,
+    SiSwagger,
+    SiFigma,
+    SiPhp,
+    SiDjango,
+    SiNestjs,
+    SiAmazonaws,
+    SiStyledcomponents,
+    SiCsharp,
+    SiBootstrap,
+} from "react-icons/si";
+import {
+    FaDocker,
+    FaHtml5,
+    FaCss3Alt,
+    FaJs,
+    FaNode,
+    FaReact,
+} from "react-icons/fa";
+
+export const stackData = [
+    {
+        title: "HTML",
+        img: FaHtml5,
+    },
+    {
+        title: "CSS",
+        img: FaCss3Alt,
+    },
+    {
+        title: "JS",
+        img: FaJs,
+    },
+    { title: "TypeScript", img: SiTypescript },
+    {
+        title: "Node JS",
+        img: FaNode,
+    },
+    {
+        title: "React",
+        img: FaReact,
+    },
+    { title: "Next.js", img: TbBrandNextjs },
+    { title: "Docker", img: FaDocker },
+    { title: "PostgreSQL", img: SiPostgresql },
+    { title: "Git", img: FaGit },
+    { title: "Python", img: FaPython },
+    {
+        title: "MongoDB",
+        img: SiMongodb,
+    },
+    {
+        title: "Swagger",
+        img: SiSwagger,
+    },
+    {
+        title: "Figma",
+        img: SiFigma,
+    },
+    {
+        title: "PHP",
+        img: SiPhp,
+    },
+    {
+        title: "Django",
+        img: SiDjango,
+    },
+    {
+        title: "Nest.js",
+        img: SiNestjs,
+    },
+    {
+        title: "Amazon Web Service EC2",
+        img: SiAmazonaws,
+    },
+    {
+        title: "Styled-Components",
+        img: SiStyledcomponents,
+    },
+    {
+        title: "C#",
+        img: SiCsharp,
+    },
+    {
+        title: "Bootstrap",
+        img: SiBootstrap,
+    },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Geferson Lopes",
+  githubUser: "GefersonLopes",
+  linkedinUser: "gefersonLopes",
+  whatsappNumber: "+5571996063783",
+  emailUser: "gefersonjefreey@gmail.com",
+  instagramUser: "gef3rs0n",
+  facebookUser: "",
+};
